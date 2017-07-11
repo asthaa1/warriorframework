70d85104ec04579e1a27f9dc5c7da674fb91c7eb
kaheichan <kaheichan@utexas.edu>
2017-07-11 16:42:05 -0500

    proof of concept complete, begin to design

--- wftests/ci/custom_rules.py
+++ wftests/ci/custom_rules.py
@@ -3,19 +3,33 @@ check your privilege...
 """
 import ast
 import sys
+import pprint
 
 f = open(sys.argv[1])
 root = ast.parse(f.read())
 for child in ast.iter_child_nodes(root):
     if isinstance(child, ast.FunctionDef):
-        print "Checking docstring in func"
+        if ast.get_docstring(child) is None:
+            print child.name, "No docstring"
         for sub_child in ast.walk(child):
             if isinstance(sub_child, ast.Print):
                 print "no print allow here"
+            if isinstance(sub_child, ast.Return):
+                print "Found a return here"
     elif isinstance(child, ast.ClassDef):
+        if ast.get_docstring(child) is None:
+            print child.name, "No docstring"
         for sub_child in ast.walk(child):
+            if isinstance(sub_child, ast.FunctionDef) and ast.get_docstring(sub_child) is None:
+                print sub_child.name, "No docstring"
             if isinstance(sub_child, ast.Print):
                 print "no print allow here"
+            if isinstance(sub_child, ast.Return):
+                print "Found a return here"
+            if isinstance(sub_child, ast.Attribute) and sub_child.attr == 'pSubStep':
+                print "Found a p"
+            if isinstance(sub_child, ast.Attribute) and sub_child.attr == 'report_substep_status':
+                print "Found a report_p"
     elif isinstance(child, ast.Str):
         print child.lineno, ":", child.s
     elif isinstance(child, ast.Expr):
